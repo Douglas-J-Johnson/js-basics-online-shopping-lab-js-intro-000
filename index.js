5b371c4520fe6ff38e454d9f94be5784e95325a1
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-04 00:59:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === Null) {
+  if (cardNumber === "") {
     return "Sorry we don\'t have a credit card on file for you."
   }
   else {
